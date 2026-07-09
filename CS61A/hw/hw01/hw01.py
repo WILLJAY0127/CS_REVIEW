e25c1e809cca3866475b6880f8c391fe13036330
WILLJAY <willjay0127@163.com>
2026-07-09 21:43:49 +0800

    c

--- CS61A/hw/hw01/hw01.py
+++ CS61A/hw/hw01/hw01.py
@@ -111,4 +111,14 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    length = 1
+    print(n)
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        print(n)
+        length += 1
+    return length
 
