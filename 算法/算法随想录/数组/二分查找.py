5f026812ad23a5dae2acd850f05cf2e370f117d5
WILLJAY <willjay0127@163.com>
2026-07-16 18:32:16 +0800

    c

--- 算法/算法随想录/数组/二分查找.py
+++ 算法/算法随想录/数组/二分查找.py
@@ -2,6 +2,7 @@
 #     def search(self, nums: List[int], target: int) -> int:    
 #         return -1
 
+# 二分查找 左闭右闭
 class Solution:
     def search(self, nums: List[int], target: int) -> int:    
         left, right = 0, len(nums) -1
@@ -13,7 +14,17 @@ class Solution:
                 right = mid - 1
             elif nums[mid] < target:
                 left = mid + 1
-                left = mid + 1
             else:
                 return mid
         return -1
+
+
+# 暴力循环
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # 暴力循环：逐个遍历每一个下标
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        # 全部遍历完都没匹配，返回-1
+        return -1        
