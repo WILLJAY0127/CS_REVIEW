43ce45bedf30363ba4d9236ea7a83b24a3532ea7
WILLJAY <willjay0127@163.com>
2026-07-16 13:50:28 +0800

    lab03

--- CS61A/lab/lab03/lab03.py
+++ CS61A/lab/lab03/lab03.py
@@ -15,6 +15,8 @@ def print_if(s, f):
     """
     for x in s:
         "*** YOUR CODE HERE ***"
+        if f(x):
+            print(x)
 
 
 def close(s, k):
@@ -33,6 +35,8 @@ def close(s, k):
     count = 0
     for i in range(len(s)):  # Use a range to loop over indices
         "*** YOUR CODE HERE ***"
+        if abs(i - s[i]) <= k:
+            count += 1
     return count
 
 
